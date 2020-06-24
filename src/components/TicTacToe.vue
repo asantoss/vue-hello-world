85dfd8dd42f1f98926bc82a85e873407cf66f094
Alexander Santos <alexsantosantana@live.com>
2020-06-24 00:27:31 -0400

    added algo to check winner status

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -4,7 +4,13 @@
     <h3 v-else>{{currentPlayer}}'s Turn!</h3>
     <button @click="resetBoard" type="reset">Reset</button>
     <div class="row" v-for="(row, i) in board" :key="i">
-      <button class="column" v-for="(col,j) in row" :key="j" @click="markColum(i, j)">{{col}}</button>
+      <button
+        v-bind:disabled="Boolean(col) || gameOver"
+        class="column"
+        v-for="(col,j) in row"
+        :key="j"
+        @click="markColum(i, j)"
+      >{{col}}</button>
     </div>
   </div>
 </template>
@@ -24,12 +30,17 @@ export default {
   }),
   methods: {
     markColum(row, column) {
-      const newBoard = [...this.board];
-      newBoard[row][column] = this.currentPlayer;
-      this.board = newBoard;
+      if (!this.board[row][column]) {
+        this.board[row][column] = this.currentPlayer;
+      } else {
+        return;
+      }
       if (this.checkWinner(this.board, this.currentPlayer)) {
         this.winner = this.currentPlayer;
         this.gameOver = !this.gameOver;
+      } else if (this.board.every(e => e.every(e => e))) {
+        this.gameOver = !this.gameOver;
+        this.winner = "no one";
       }
       this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
     },
@@ -42,46 +53,54 @@ export default {
       this.gameOver = false;
       this.winner = null;
     },
-    checkWinner(board, currentPlayer) {
-      let items;
-      for (let i = 0; i < board.length; i++) {
-        const row = board[i];
-        if (items === currentPlayer.repeat(3)) {
-          return true;
-        } else {
-          items = "";
-        }
+    checkWinner() {
+      let items = 0;
+      for (let i = 0; i < this.board.length; i++) {
+        const row = this.board[i];
         for (let j = 0; j < row.length; j++) {
           const col = row[j];
-          if (col === currentPlayer) {
-            items += col;
+          console.log({ x: i, y: j, items });
+          if (col === this.currentPlayer) {
+            items += 1;
+          } else {
+            items = 0;
           }
         }
-      }
-      for (let i = 0; i < board.length; i++) {
-        const col = board[i][i];
-        if (col === currentPlayer) {
-          items += col;
-        } else {
-          items = "";
-        }
-        if (items === currentPlayer.repeat(3)) {
+        if (items === 3) {
           return true;
         }
       }
-
-      for (let i = 0; i < board.length; i++) {
-        const row = board[i];
-        for (let j = 0; j < row.length; j++) {
-          const col = board[j][i];
-          if (col === currentPlayer) {
-            items += currentPlayer;
-          } else {
-            items = "";
-          }
-          if (items === currentPlayer.repeat(3)) {
-            return true;
-          }
+      for (let i = 0; i < this.board.length; i++) {
+        const col = this.board[i][i];
+        if (col === this.currentPlayer) {
+          items += 1;
+        } else {
+          items = 0;
+        }
+        if (items === 3) {
+          return true;
+        }
+      }
+      for (let i = 0; i < this.board.length; i++) {
+        const col = this.board[i][0];
+        if (col === this.currentPlayer) {
+          items += 1;
+        } else {
+          items = 0;
+        }
+        if (items === 3) {
+          return true;
+        }
+      }
+      for (let i = 0; i < this.board.length; i++) {
+        const col = this.board[i][this.board.length - 1 - i];
+        if (col === this.currentPlayer) {
+          items += 1;
+        } else {
+          items = 0;
+        }
+        if (items === 3) {
+          return true;
         }
       }
     }
@@ -89,7 +108,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style  scoped>
 .row {
   display: flex;
   margin: auto;
@@ -98,5 +117,10 @@ export default {
 .column {
   width: 50px;
   height: 50px;
+}
+.column:disabled {
+  font-weight: bold;
+  color: black;
+  border: 1px solid red;
 }
 </style>
